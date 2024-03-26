c64ec1bb346198167831d3420b8edca9dde1e983
igl-myrick <igl.myrick@gmail.com>
2024-03-26 14:52:38 -0700

    add calculatePrice prototype

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,14 @@ Pizza.prototype.addTopping = function(inputTopping) {
 Pizza.prototype.setSize = function(inputSize) {
   this.size = inputSize;
 }
+
+Pizza.prototype.calculatePrice = function() {
+  let result = 0;
+  for (let i = 0; i < this.toppings.length; i+=1) {
+    result += 0.5;
+  }
+  return result;
+}
+
+// business logic
+let userPizza = new Pizza();
