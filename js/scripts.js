91b3f0dcf14a38a817c68e2f6bd4f28c8622e826
igl-myrick <igl.myrick@gmail.com>
2024-03-27 14:24:17 -0700

    add initial ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -24,8 +24,39 @@ Pizza.prototype.calculatePrice = function() {
   } else {
     result += 12;
   }
-  return result;
+  return result.toFixed(2);
 }
 
 // business logic
 let userPizza = new Pizza();
+
+// ui logic
+function displayUserPizza(pizzaSize, pizzaToppings) {
+  const pizzaDisplayDiv = document.createElement("div");
+  const pizzaDisplayPara = document.createElement("p");
+  const pizzaDisplayList = document.createElement("ul");
+  pizzaDisplayPara.innerText = "You have selected a " + pizzaSize + " pizza with:"
+  pizzaToppings.forEach(function(element) {
+    const listItem = document.createElement("li");
+    listItem.innerText = element;
+    pizzaDisplayList.append(listItem);
+  });
+  pizzaDisplayDiv.append(pizzaDisplayPara);
+  pizzaDisplayDiv.append(pizzaDisplayList);
+  document.body.append(pizzaDisplayDiv);
+}
+
+function getPizzaInfo(e) {
+  e.preventDefault();
+  const pizzaSize = document.querySelector("input[name='size']:checked").value;
+  userPizza.setSize(pizzaSize);
+  const selectedToppings = document.querySelectorAll("input[name='topping']:checked");
+  selectedToppings.forEach(function(element) {
+    userPizza.addTopping(element.value);
+  });
+  displayUserPizza(userPizza.size, userPizza.toppings);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form").addEventListener("submit", getPizzaInfo);
+})
